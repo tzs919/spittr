316895539c9c8d59162f3c398811a907ee1c18ae
tzs919 <tzs919@163.com>
2021-03-12 10:15:16 +0800

    加入基于数据库用户的认证

--- src/main/java/spittr/config/SecurityConfig.java
+++ src/main/java/spittr/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,46 +9,62 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .formLogin()
-        .loginPage("/login")
-      .and()
-        .logout()
-          .logoutSuccessUrl("/")
-      .and()
-      .rememberMe()
-        .tokenRepository(new InMemoryTokenRepositoryImpl())
-        .tokenValiditySeconds(2419200)
-        .key("spittrKey")
-      .and()
-       .httpBasic()
-         .realmName("Spittr")
-      .and()
-      .authorizeRequests()
-        .antMatchers("/").hasAnyRole("USER")
-        .antMatchers("/spitter/me").authenticated()
-        .antMatchers(HttpMethod.POST, "/spittles").authenticated()
-        .anyRequest().permitAll()
-    .and()
-            .requiresChannel()
-            .antMatchers("/spitter/register").requiresSecure()
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .requiresChannel()
+//                .antMatchers("/").requiresSecure()
+//                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .rememberMe()
+                .tokenRepository(new InMemoryTokenRepositoryImpl())
+                .tokenValiditySeconds(2419200)
+                .key("spittrKey")
+                .and()
+                .httpBasic()
+                .realmName("Spittr")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").hasAnyRole("USER")
+                .antMatchers("/spitter/me").authenticated()
+                .antMatchers(HttpMethod.POST, "/spittles").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .requiresChannel()
+                .antMatchers("/spitter/register").requiresSecure()
 //    .and().csrf().disable()
-    ;
+        ;
 
-  }
+    }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-      .inMemoryAuthentication()
-        .withUser("user").password("password").roles("USER");
-  }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    auth
+//      .inMemoryAuthentication()
+//        .withUser("user").password("password").roles("USER");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password,true from Spitter where username=? ")
+                .authoritiesByUsernameQuery(
+                        "select username,'ROLE_USER' from Spitter where username=?"
+                );
+    }
 
 
 }
